a74b54f08206c633c9146a165d803ef595063359
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2021-03-05 09:40:07 +0100

    updates

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Bot is running"
+# s1="git init"
+s2="git pull"
+s21="git add ."
+s22="git commit -m \"updates\""
+s3="git push --all"
+for ((counter=1200;counter>0;counter--))
+do 
+echo $counter
+(eval $s2)
+(eval $s21)
+(eval $s22)
+(eval $s3)
+sleep 30
+done
+echo "ferdig"
